974df4c04e83395f6d780c5533d789d9ecf4760d
tlee10333 <trinityl10333@gmail.com>
2023-10-24 14:12:03 -0400

    Organized firmware and hardware tester code

---
+++ motor firmware/main-code/main-code.ino
@@ -0,0 +1,47 @@
+
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *motor_l = AFMS.getMotor(4);
+
+
+void setup() {  
+
+
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
+  Serial.println("Motor Shield found.");  
+
+  motor_l->setSpeed(30);
+
+}
+
+void loop() {
+  motor_l->setSpeed(30);
+
+  motor_l-> run(FORWARD);
+  delay(100);
+
+  //motor_l-> run(BACKWARD);
+  //delay(100);
+
+
+
+
+
+
+
+   
+
+}
